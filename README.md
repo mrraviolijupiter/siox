d20a43690c93e009a728711f1d7e64eca06a4d73
mateosss <mateodemayo@gmail.com>
2020-01-18 16:01:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Socket IO Example for darsia testing
+
+# Run server
+
+```
+npm install
+npm start
+```
+
+# Run client
+
+open `client.html`
+
+
---
+++ server.js
@@ -0,0 +1,47 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const util = require('util');
+const port = 1337;
+const clients = [];
+
+// Start the server
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+// Serve web client
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/client.html');
+});
+
+// When a client connects, bind each desired event to the client socket
+io.on('connection', function(socket){
+	//track connected clients via log
+	clients.push(socket.id);
+  const clientConnectedMsg = `User connected with socket.id=${socket.id}, total: ${clients.length}`;
+	io.emit('server_info', clientConnectedMsg);
+	console.log("CLIENT CONNECTED: ", clientConnectedMsg);
+
+  // Callback for any received message
+  socket.use((packet, next) => {
+    console.log("PACKET RECEIVED: ", packet);
+    return next();
+  });
+
+	// track disconnected clients via log
+	socket.on('disconnect', function(){
+		clients.pop(socket.id);
+    const clientDisconnectedMsg = `User disconnected with socket.id=${socket.id}, total: ${clients.length}`;
+		io.emit('server_info', clientDisconnectedMsg);
+		console.log("CLIENT DISCONNECTED: ", clientDisconnectedMsg);
+	})
+
+	// special event to make the server give its next message
+  // modify this for custom responses
+	socket.on('next_command', function(msg){
+    const next = { a: { a: { a: 1 } }, b: 2 };
+		io.emit('next_command', JSON.stringify(next, null, "  "));
+    console.log("NEXT SENT: ", JSON.stringify(next, null, "  "));
+	});
+});
